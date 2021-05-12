3d0017674119f0961f96bb88b3129755523a48fe
Thaekeh <thaekeh@gmail.com>
2021-05-13 00:53:33 +0200

    Sessionrow overhaul

--- src/components/SessionRow.vue
+++ src/components/SessionRow.vue
@@ -1,31 +1,46 @@
 <template>
-    <tr>
-        <td>
-            {{ session.title }}
-        </td>
-        <td>
-            {{ session.id }}
-        </td>
-        <td>
-            {{ formatDate(session.startTime) }}
-        </td>
-    </tr>
+  <tr>
+    <td>
+      {{ session.title }}
+    </td>
+    <td>
+      {{ formatDate(session.startTime) }}
+    </td>
+    <td>
+      {{ formatDate(session.endTime) }}
+    </td>
+    <td>
+      {{ formatTimeDifference(session.totalTime) }}
+    </td>
+  </tr>
 </template>
 
 <script>
 export default {
-    name: "SessionRow",
-    props: ['session'],
-    methods: {
-        formatDate(time) {
-            const dateObj = new Date(time);
-            const hours = dateObj.getHours();
-            const minutes = dateObj.getMinutes();
-            const seconds = dateObj.getSeconds();
-            return `${hours}:${minutes}:${seconds}`;
-            // return new Date(time).getUTCHours();
-            // new Date(val[0].value).toLocaleFormat('%m/%d/%Y')
-        }
+  name: "SessionRow",
+  props: ["session"],
+  methods: {
+    formatDate(time) {
+      const dateObj = new Date(time);
+      const hours = dateObj.getHours();
+      const minutes = dateObj.getMinutes();
+      const seconds = dateObj.getSeconds();
+      return `${this.replaceSingleNumber(hours)}:${this.replaceSingleNumber(minutes)}:${this.replaceSingleNumber(seconds)}`;
+    },
+    formatTimeDifference(timeInMilliSeconds) {
+      const timeInSeconds = timeInMilliSeconds / 1000;
+      const hours = Math.floor(timeInSeconds / 3600);
+      const minutes = Math.floor((timeInSeconds % 3600) / 60);
+      const seconds = Math.floor(timeInSeconds % 60);
+      return `${this.replaceSingleNumber(hours)}:${this.replaceSingleNumber(minutes)}:${this.replaceSingleNumber(seconds)}`;
+    },
+    replaceSingleNumber(number) {
+      if (this.isSingleDigit(number)) return `0${number}`;
+      return number;
+    },
+    isSingleDigit(number) {
+        return String(+number).charAt(0) == number;
     }
-}
+  },
+};
 </script>
