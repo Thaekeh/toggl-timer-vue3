0ba3c4f521ea1809c885f46351c956ca7d0e9a18
Thaekeh <thaekeh@gmail.com>
2021-05-13 00:21:44 +0200

    Table row for session

---
+++ src/components/SessionRow.vue
@@ -0,0 +1,31 @@
+<template>
+    <tr>
+        <td>
+            {{ session.title }}
+        </td>
+        <td>
+            {{ session.id }}
+        </td>
+        <td>
+            {{ formatDate(session.startTime) }}
+        </td>
+    </tr>
+</template>
+
+<script>
+export default {
+    name: "SessionRow",
+    props: ['session'],
+    methods: {
+        formatDate(time) {
+            const dateObj = new Date(time);
+            const hours = dateObj.getHours();
+            const minutes = dateObj.getMinutes();
+            const seconds = dateObj.getSeconds();
+            return `${hours}:${minutes}:${seconds}`;
+            // return new Date(time).getUTCHours();
+            // new Date(val[0].value).toLocaleFormat('%m/%d/%Y')
+        }
+    }
+}
+</script>
