9878024bb32ed870e6515c87a6039bf43dde1864
Thaekeh <thaekeh@gmail.com>
2021-05-12 23:34:14 +0200

    Added saved sessions

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <template>
+<div id="app">
   <TimerBar />
+  <SavedSessions />
+</div>
 </template>
 
 <script>
 import TimerBar from './components/TimerBar.vue'
+import SavedSessions from './components/SavedSessions.vue'
 
 export default {
   name: 'App',
   components: {
-    TimerBar
+    TimerBar,
+    SavedSessions
   }
 }
 </script>
---
+++ src/components/SavedSessions.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <table>
+            <td>
+                Title
+            </td>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SavedSessions'
+}
+</script>
